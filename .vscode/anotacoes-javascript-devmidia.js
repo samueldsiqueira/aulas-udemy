78aced1fb086e780236e2810eb8eda70a6958d69
Samuel D Siqueira <samueldsiqueira@gmail.com>
2020-01-14 01:12:19 -0300

    3-horas-de-codigo

---
+++ .vscode/anotacoes-javascript-devmidia.js
@@ -0,0 +1,40 @@
+// hoje ganhei acesso a plataforma dev midia até dia 08/01.
+
+//Isso é um evento no browser que necessita de uma programação. Isso só pode ser feito com JavaScript.
+
+//Tecnicamente, dizemos a mesma coisa assim: o JavaScript é a única linguagem que pode manipular o DOM.
+
+//DOM é a sigla para Document Object Model
+
+//Em palavras simples, quando falamos DOM queremos dizer o seguinte: que o JavaScript permite manipular elementos HTML como se eles fossem objetos.
+
+//Por exemplo, ao clicar no objeto botão(HTML = elemento < button > elemento < /button>) um menu deve ser aberto (HTML = elemento <nav>), como vemos na Animação 2.
+
+
+// ahh, as anotações no java são feitas com duas barras inclinadas para direita e são feitas por linha, não sendo uma tag necessário abrir ou fechar.
+
+//Com JavaScript e DOM conseguimos:
+
+//Alterar os elementos da página;
+
+//Alterar os estilos, ou seja, cores, tamanho de fonte, …
+
+//Excluir ou acrescentar um novo elemento;
+
+//Criar SPAs, ou aplicações de página única, onde alteramos toda estrutura, sem recarregar a página.
+
+//Para se aprofundar nesse assunto recomendamos o uso de um framework, tais como o Angular, Vue.js ou React.
+
+
+// !!!IMPORTANTE!!! aprender javascript vai ser meu primeiro passo para poder aprender angular, react e etc. !!!IMPORTANTE!!!
+
+//O JavaScript é fullstack porque ele consegue criar os principais tipos de aplicação, que são:
+
+//Aplicações front - end;
+//Aplicações back - end;
+//Aplicações mobile
+
+// finalmente sei o que é fullstack
+
+
+//O JavaScript é a principal linguagem na programação front - end porque é a única linguagem que roda nativamente no navegador.
